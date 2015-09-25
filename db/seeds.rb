5aa93ffd2392dc18426d8e3f0184f13f800bca8a
Blair Johnson <helloblairj@gmail.com>
2015-09-25 16:42:16 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user1 = User.new({ :first_name => "Blair", :last_name => "Johnson", :email => "helloblairj@gmail.com", :password => "cookie" })
-user2 = User.new({ :first_name => "Steve", :last_name => "Deddens", :email => "sdeddens@gmail.com", :password => "cookie" })
-user3 = User.new({ :first_name => "Rohit", :last_name => "Mahnot", :email => "rohitmahnot92@gmail.com", :password => "cookie" })
-user4 = User.new({ :first_name => "Akash", :last_name => "Shekara", :email => "akashshekara@gmail.com", :password => "cookie" })
-#user5 = User.new({ :first_name => "Cookie", :last_name => "Monster", :email => "cookiemonste34234", :password => "cookie" })
 
+# user = User.new({ :first_name => "Blair", :last_name => "Johnson", :email => "helloblairj@gmail.com", :password => "cookie" })
+# user.save
+
+# user = User.new({ :first_name => "Steve", :last_name => "Deddens", :email => "sdeddens@gmail.com", :password => "cookie" })
+# user.save
+
+# user = User.new({ :first_name => "Rohit", :last_name => "Mahnot", :email => "rohitmahnot92@gmail.com", :password => "cookie" })
+# user.save
+
+# user = User.new({ :first_name => "Akash", :last_name => "Shekara", :email => "akashshekara@gmail.com", :password => "cookie" })
+# user.save
+
+# user = User.new({ :first_name => "Cookie", :last_name => "Monster", :email => "cookiemonste34234", :password => "cookie" })
+# user.save
+
+# user = User.new({ :first_name => "Joe", :last_name => "Ahlert", :email => "joe.ahlert@gmail.com", :password => "hello" })
+# user.save
+
+# user.hash_password
 # update posts/URL
+
+
+# post = Post.new({ :url => "http://www.builtinaustin.com/job/front-end-developer-5"})
+# post.save
+
+
+post = Post.new({ :url => "http://www.jobs2careers.com/results.php?q=developer&l=Austin%2C+TX"})
+post.save
